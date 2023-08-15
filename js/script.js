9d09cd38a69d4da9e3531915e5af42c8819396cf
Zombear323 <129801201+Zombear323@users.noreply.github.com>
2023-08-15 17:04:30 -0400

    code added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 var APIKey = "ae4782e7b7c232a81f8182096d5d5a5e"
-var city = 'Toronto'
-var queryURL  = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey
+var cityinput = ['toronto']
+var queryURL  = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityinput + '&units=metric&appid=' + APIKey
+var dayofcard = $('.dayof')
+var fivedaycard = $('.fiveday')
+  
 
-fetch(queryURL);
+
+fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        console.log('Weather: Raw data \n----------');
+        console.log(data);
+        for (var i = 0; i < 6; i++) {
+            console.log(data.list[i].main.temp);
+            console.log(data.list[i].main.humidity);
+            console.log(data.list[i].wind.speed);
+            console.log(data.list[i].weather[0].main);
+            var iconcode = data.list[i].weather[0].icon
+            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png"; 
+            console.log(iconurl)
+            // if (i = 0) {
+              // create dayof forecast
+            // }
+            // else {
+              // create 5day forcast
+            // }
+          }
+    });
+
+
+
+
+
+// var SearchButton = $('')
+
+// SearchButton.on("click", function(event) {
+//   event.preventDefault();
+//   var cityinput = $(this).siblings(".description").val()
+//   console.log("test",svdnote,timeBlk);
+//   localStorage.setItem()
+      
+//   });
+
+
